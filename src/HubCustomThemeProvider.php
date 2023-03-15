694de7025e7363c7f8c2c180afb9c7ca88a2f5b6
Adam Lee <adam@codelabx.ltd>
2023-03-15 17:50:54 +0000

    Initial commit

---
+++ src/HubCustomThemeProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace XtendLunar\Features\HubCustomTheme;
+
+use CodeLabX\XtendLaravel\Base\XtendFeatureProvider;
+
+class HubCustomThemeProvider extends XtendFeatureProvider
+{
+    /**
+     * Extends register service provider
+     *
+     * @return void
+     */
+    public function withRegister(): void
+    {
+        //
+    }
+
+    /**
+     * Extends boot service provider
+     *
+     * @return void
+     */
+    public function withBoot(): void
+    {
+
+    }
+}
