4e1e2d8c573ac59ecce6230298c582b81e23e8b0
Adam Lee <adam@codelabx.ltd>
2023-04-20 15:52:21 +0100

    Only load hub styles for product show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,10 @@
     @vite(['resources/css/hub-extend.css', 'resources/js/hub-extend.js'])
 
     <!-- Note: We do not use the default app.css file, but instead use the Vite version. -->
-    {{--<link href="{{ asset('vendor/lunar/admin-hub/app.css?v=1') }}" rel="stylesheet">--}}
+    <!-- @todo Styles conflict especially with filament tables so only use this for product section -->
+    @if (request()->routeIs('hub.products.show'))
+        <link href="{{ asset('vendor/lunar/admin-hub/app.css?v=1') }}" rel="stylesheet">
+    @endif
 
     @if ($styles = \Lunar\Hub\LunarHub::styles())
         @foreach ($styles as $asset)
